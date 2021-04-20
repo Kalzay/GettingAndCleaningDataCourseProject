35045fe63aa8f4b5c07398925a9748ddb59d6861
Kalzay <jon.robert.hall97@gmail.com>
2021-04-20 18:20:22 +0100

    Adding code to download data if it does not exist

--- run_analysis.r
+++ run_analysis.r
@@ -3,6 +3,13 @@ library(readr)
 library(dplyr)
 library(tidyr)
 
+# Download data
+fileurl = 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+if (!file.exists('./UCI HAR Dataset.zip')){
+        download.file(fileurl,'./UCI HAR Dataset.zip', mode = 'wb')
+        unzip("UCI HAR Dataset.zip", exdir = getwd())
+}
+
 # Setting the data dictionary
 data_dir <- "./UCI HAR Dataset/"
 
